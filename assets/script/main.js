b9f28b375c869dacd47c77d144e972ad96016621
youko.honda <kyogranatus@jcom.home.ne.jp>
2024-06-18 20:41:09 +0900

    asset追加

---
+++ assets/script/main.js
@@ -0,0 +1,19 @@
+// ハンバーガーメニュー
+var hamburger = $('.hamburger-menu');
+
+
+$('.hamburger-button').on('click', function () {
+  hamburger.toggleClass('hamburger-menu-active');
+});
+
+$(window).on('resize', function () {
+  hamburger.removeClass('hamburger-menu-active');
+});
+
+// タブ
+$('#smarttab').smartTab({
+  enableUrlHash: false,
+  autoAdjustHeight: false
+});
+
+
